adbf00bc3b94fbe203f81f86039cb1d4e6249f28
Sena Kim <sena.kim2112@gmail.com>
2026-05-19 14:08:54 -0400

    day_1_ generate band name

---
+++ Day1/bandname.py
@@ -0,0 +1,4 @@
+print("Welcome to BAND NAME ☆ GENERATOR!")
+city_name = input("Which city did you grow up in?")
+pet_name = input("What is your pet name?")
+print("Congratulations! \n Your Band name is " + city_name + " " + pet_name + ("!"))
